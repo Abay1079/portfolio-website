365ec7bfecdc3a3cd75581212559dd273498e01e
Abay1079 <hulivia96@gmail.com>
2025-05-14 13:05:15 -0400

    Update script.js
    add dom back to top

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
   window.onload = function() {
     console.log("Welcome to my portfolio site!");
 
+// back to top
+    function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
